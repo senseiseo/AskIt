20d217cc23187a5514d1a3286eba1cd28a41f895
Senseiseo <1919mur@gmail.com>
2022-01-27 13:10:15 +0300

    add user role enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  attr_accessor :old_password, :remember_token
+  enum role: {basic: 0, moderator: 1, admin: 2}, _suffix: :role
+
+  attr_accessor :old_password, :remember_token, :admin_edit
 
   has_secure_password validations: false
 
@@ -9,7 +11,7 @@ class User < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   validate :password_presence
-  validate :correct_old_password, on: :update, if: -> { password.present? }
+  validate :correct_old_password, on: :update, if: -> { password.present? && !admin_edit}
   validates :password, confirmation: true, allow_blank: true,
                        length: { minimum: 8, maximum: 70 }
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_26_064517) do
+ActiveRecord::Schema.define(version: 2022_01_27_073616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,7 +69,9 @@ ActiveRecord::Schema.define(version: 2022_01_26_064517) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "remember_token_digest"
     t.string "gravatar_hash"
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["role"], name: "index_users_on_role"
   end
 
   add_foreign_key "answers", "questions"
